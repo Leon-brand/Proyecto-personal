a00c2a0d31aed311a85166d745e22fc2edf0b8e4
Your Name <you@example.com>
2021-04-08 13:37:06 -0500

    se actualiza otra vez el titulo del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'La app de Leon';
+  title = 'La app de Leon para volverme mejor';
 }
